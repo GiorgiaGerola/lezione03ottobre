e686db26e98ae2522391340b0505374d8e9b6434
Giorgia Gerola <Giorgia.Gerola@marconi.loc>
2018-10-10 16:12:24 +0200

    esempio 5.8

--- routes/api_router.js
+++ routes/api_router.js
@@ -5,14 +5,18 @@ var ALLOWED_IPS = [
 ];
 var api = express.Router();
 api.use(function(req, res, next) {
+    console.log(req.ip);
     var userIsAllowed = ALLOWED_IPS.indexOf(req.ip) !== -1;
-    if (!userIsAllowed) {
+    /*if (!userIsAllowed) {
     res.status(401).send("Not authorized!");
-    } else {
+    } else */{
     next();
     }
 });
-api.get("/users", function(req, res) { /* ... */ });
+api.get("/users", function(req, res) { 
+    console.log(req.url);
+    res.end(req.url); 
+});
 api.post("/user", function(req, res) { /* ... */ });
 api.get("/messages", function(req, res) { /* ... */ });
 api.post("/message", function(req, res) { /* ... */ });
